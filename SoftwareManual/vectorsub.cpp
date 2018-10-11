1937858d38ae54e6a28a15133494cdac2acc901b
Jaredcl1994 <32687987+Jaredcl1994@users.noreply.github.com>
2018-10-11 17:47:53 -0600

    Update vectorsub.cpp

--- SoftwareManual/vectorsub.cpp
+++ SoftwareManual/vectorsub.cpp
@@ -1,3 +1,52 @@
+# Math 4610 Fundamentals of Computational Mathematics Software Manual VECTORSUB File
+
+**Routine Name:**           vectorsub  
+
+**Author:** Jared Lambert
+
+**Language:** C++, the code can be compiled using the GNU C++ compiler (g++). 
+
+For example,
+
+    g++ -o vectorsub.cpp  
+
+**Description/Purpose:** This routine gives you the difference of two vectors.     
+    
+
+
+**Input:** There are two inputs, both of type std::vector<double>.  
+    
+
+**Output:** returns an std::vector<double> which is the difference of the two input vectors.  
+  
+
+**Usage/Example:**
+You will call the function with two arguments, as previously stated, like:
+```c++
+int main()
+{
+  std::vector<double> a = {1,2,3};
+  std::vector<double> b = {0,1,2};
+  std::vector<double> c = vectorsub(a,b);
+  for(auto x:c)
+    {
+      std::cout << x << std::endl;
+    }
+  return 0;
+}
+```
+and the output for this line will look like:    
+```
+1
+1
+1
+```  
+
+which is the difference.    
+
+**Implementation/Code:**  
+
+```c++
 #include <iostream>
 #include <cmath>
 #include <assert.h>
@@ -15,16 +64,6 @@ std::vector<double>  vectorsub(std::vector<double> a, std::vector<double> b)
     }
   return c;
 }
-/*
-int main()
-{
-  std::vector<double> a = {1,2,3};
-  std::vector<double> b = {0,1,2};
-  std::vector<double> c = vectorsub(a,b);
-  for(auto x:c)
-    {
-      std::cout << x << std::endl;
-    }
-  return 0;
-}
-*/
+```  
+  
+**Last Modified:** October/2018
