997ed6d86cc1309e880ffa32d797aea91875b153
Jaredcl1994 <32687987+Jaredcl1994@users.noreply.github.com>
2018-10-11 21:08:46 -0600

    Update transpose.cpp

--- SoftwareManual/transpose.cpp
+++ SoftwareManual/transpose.cpp
@@ -1,3 +1,57 @@
+# Math 4610 Fundamentals of Computational Mathematics Software Manual TRANSPOSE File
+
+**Routine Name:**           transpose
+
+**Author:** Jared Lambert
+
+**Language:** C++, the code can be compiled using the GNU C++ compiler (g++). 
+
+For example,
+
+    g++ -o vect transpose.cpp
+
+**Description/Purpose:** This routine gives you the transpose of a matrix.     
+    
+
+
+**Input:** There is one input, a matrix of type std::vector<std::vector<double>>.  
+    
+
+**Output:** returns an std::vector<std::vector<double>> matrix which is the transpose an input matrix.  
+  
+
+**Usage/Example:**
+You will call the function with one argument, as previously stated, like:  
+```c++
+int main()
+{
+  std::vector<std::vector<double>> a{{1,2,4}, {1,2,5}, {6,2,6}};
+  
+  std::vector<std::vector<double>> c = transpose(a);
+  for(int i = 0; i < c.size(); i ++)
+    {
+      for(int j = 0; j < c[i].size(); j++)
+	{
+	  std::cout << c[i][j] << " ";
+	}
+      std::cout <<  std::endl;
+    }
+  return 0;
+}
+```
+and the output for this line will look like:    
+```
+1 1 6
+2 2 2
+4 5 6
+```  
+
+which is the transpose.
+
+**Implementation/Code:**  
+
+```c++
+
 #include <iostream>
 #include <cmath>
 #include <assert.h>
@@ -24,19 +78,14 @@ std::vector<std::vector<double>>  transpose(std::vector<std::vector<double>> a)
   return c;
 }
 
-int main()
-{
-  std::vector<std::vector<double>> a{{1,2,4}, {1,2,5}, {6,2,6}};
-  
-  std::vector<std::vector<double>> c = transpose(a);
-  for(int i = 0; i < c.size(); i ++)
-    {
-      for(int j = 0; j < c[i].size(); j++)
-	{
-	  std::cout << c[i][j] << " ";
-	}
-      std::cout <<  std::endl;
-    }
-  return 0;
-}
+
+```
+
+**Last Modified:** October/2018
+
+
+
+
+
+
 
