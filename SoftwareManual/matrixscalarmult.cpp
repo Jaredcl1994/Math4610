cf19e24b9abaae10081f53054906b62abebeaaef
Jaredcl1994 <32687987+Jaredcl1994@users.noreply.github.com>
2018-10-11 21:02:43 -0600

    Update matrixscalarmult.cpp

--- SoftwareManual/matrixscalarmult.cpp
+++ SoftwareManual/matrixscalarmult.cpp
@@ -1,3 +1,57 @@
+# Math 4610 Fundamentals of Computational Mathematics Software Manual MATRIXSCALARMULT File
+
+**Routine Name:**           matrixscalarmult
+
+**Author:** Jared Lambert
+
+**Language:** C++, the code can be compiled using the GNU C++ compiler (g++). 
+
+For example,
+
+    g++ -o vect matrixscalarmult.cpp
+
+**Description/Purpose:** This routine gives you the product of a double and a matrix.     
+    
+
+
+**Input:** There are two inputs, one double and a matrix of type std::vector<std::vector<double>>.  
+    
+
+**Output:** returns an std::vector<std::vector<double>> matrix which is the product of a double and an input matrix.  
+  
+
+**Usage/Example:**
+You will call the function with two arguments, as previously stated, like:  
+```c++
+int main()
+{
+  std::vector<std::vector<double>> a{{1,2,4}, {1,2,5}, {6,2,6}};
+  
+  std::vector<std::vector<double>> c = matrixscalarmult(3.0, a);
+  for(int i = 0; i < c.size(); i ++)
+    {
+      for(int j = 0; j < c[i].size(); j++)
+	{
+	  std::cout << c[i][j] << " ";
+	}
+      std::cout <<  std::endl;
+    }
+  return 0;
+}
+```
+and the output for this line will look like:    
+```
+3 6 12
+3 6 15
+18 6 18
+```  
+
+which is the product.
+
+**Implementation/Code:**  
+
+```c++
+
 #include <iostream>
 #include <cmath>
 #include <assert.h>
@@ -23,19 +77,10 @@ std::vector<std::vector<double>>  matrixscalarmult(double b, std::vector<std::ve
   return c;
 }
 
-int main()
-{
-  std::vector<std::vector<double>> a{{1,2,4}, {1,2,5}, {6,2,6}};
-  
-  std::vector<std::vector<double>> c = matrixscalarmult(3.0, a);
-  for(int i = 0; i < c.size(); i ++)
-    {
-      for(int j = 0; j < c[i].size(); j++)
-	{
-	  std::cout << c[i][j] << " ";
-	}
-      std::cout <<  std::endl;
-    }
-  return 0;
-}
+
+```
+
+**Last Modified:** October/2018
+
+
 
